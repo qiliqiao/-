030eaeca865dacca222e202d819f11b9c475fb66
qiliqiao <qiliqiao@qideMacBook-Air.local>
2016-10-26 09:42:21 +0900

    OK
    OK

---
+++ TD-Test/Assets/osu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class osu : MonoBehaviour 
+{
+
+	public int M_number;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Debug.Log (M_number);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		ClickTest ();
+	}
+
+	public void ClickTest()
+	{
+		Debug.Log (M_number);
+	}
+}
